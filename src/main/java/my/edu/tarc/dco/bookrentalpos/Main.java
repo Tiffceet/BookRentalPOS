e2aa88c474dd0009f4bb3d27983c4aafa936662d
Tiffceet <zkang.loo@gmail.com>
2020-07-15 00:16:15 +0800

    Fixing all the gibberish

--- src/main/java/my/edu/tarc/dco/bookrentalpos/Main.java
+++ src/main/java/my/edu/tarc/dco/bookrentalpos/Main.java
@@ -9,27 +9,8 @@ public class Main {
     public static void main(String[] args) {
         
         DBManager db = new DBManager();  
-	StaffManager stf = new StaffManager(db);
-	stf.registerStaff(new Staff("loo32z", "84329df"));
-
-        // dadsljk
-        // szesing add code here
-        // test
-//f sdhjfdshf
-        dsfkjsdhf
-        ds
-        af
-        ds
-        dfs
-        fs
-        fsd
-        f
-        sfs
-        d
-        
-
-        dlksadjklas
-ffdfddbc3a5525ced55e5472fcc029ecb7345ded
+		StaffManager stf = new StaffManager(db);
+		stf.registerStaff(new Staff("loo32z", "84329df"));
 
     }
 }
