5f2f69423c65205b6c8af842193c8b06b7776071
Tiffceet <zkang.loo@gmail.com>
2022-09-15 17:56:01 +0800

    Add: book setup

--- src/main/java/bookrentalpos/ManageOrganizationController.java
+++ src/main/java/bookrentalpos/ManageOrganizationController.java
@@ -1,16 +1,31 @@
 package bookrentalpos;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.*;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.*;
 
-public class ManageOrganizationController {
+public class ManageOrganizationController implements Initializable {
     @FXML
     private Button backButton;
+
+    @FXML
+    private ChoiceBox<String> systemLangComboBox;
+    @FXML
+    private ChoiceBox<String> currencyComboBox;
+    @FXML
+    private ChoiceBox<String> bookType1;
+    @FXML
+    private ChoiceBox<String> bookType2;
+    @FXML
+    private ChoiceBox<String> bookType3;
 
     public void backToMain(MouseEvent event) throws IOException {
         Parent mainMenuParent = FXMLLoader.load(getClass().getResource("/FXML/mainMenu.fxml"));
@@ -19,5 +34,14 @@ public class ManageOrganizationController {
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setTitle("Main Menu - HuaheeCheh");
         window.setScene(mainMenuScene);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        systemLangComboBox.setItems(FXCollections.observableArrayList("English", "Malay", "Chinese"));
+        currencyComboBox.setItems(FXCollections.observableArrayList("MYR", "USD", "GBP"));
+        bookType1.setItems(FXCollections.observableArrayList("Fiction", "Recipe", "Personal Planning"));
+        bookType2.setItems(FXCollections.observableArrayList("Fiction", "Recipe", "Personal Planning"));
+        bookType3.setItems(FXCollections.observableArrayList("Fiction", "Recipe", "Personal Planning"));
     }
 }
